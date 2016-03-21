1321531135010b50f304dd60897da130b93ef2d0
Gary Jones <gary@gamajo.com>
2016-03-21 12:42:14 +0000

    Code standards
    * Add missing documentation.
    * Re-order methods to move public first.

--- src/Redirects.php
+++ src/Redirects.php
@@ -1,104 +1,122 @@
 <?php
+/**
+ * Redirects module for Codeception.
+ *
+ * @package   gamajo\codeception-redirects
+ * @author    Gary Jones
+ * @copyright 2016 Gamajo Tech
+ * @license   MIT
+ */
 
 namespace Codeception\Module;
 
 use Codeception\Module;
 
-class Redirects extends Module{
-
+/**
+ * Class Redirects
+ *
+ * @package Codeception\Module
+ * @author  Gary Jones
+ */
+class Redirects extends Module
+{
     /**
-     * Protocol constants that can be used with permanentRedirectForProtocol() method.
+     * Protocol (http) constant that can be used with permanentRedirectForProtocol() method.
      */
     const PROTOCOL_HTTP = 'http';
+
+    /**
+     * Protocol (https) constant that can be used with permanentRedirectForProtocol() method.
+     */
     const PROTOCOL_HTTPS = 'https';
 
-	/**
-	 * Check that a 301 HTTP Status is returned with the correct Location URL.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $url Relative or absolute URL of redirect destination.
-	 */
-	public function seePermanentRedirectTo($url)
-	{
-		$response       = $this->getModule('PhpBrowser')->client->getInternalResponse();
-		$responseCode   = $response->getStatus();
-		$locationHeader = $response->getHeader('Location', true);
+    /**
+     * Toggle redirections on and off.
+     *
+     * By default, BrowserKit will follow redirections, so to check for 30*
+     * HTTP status codes and Location headers, they have to be turned off.
+     *
+     * @since 0.1.0
+     *
+     * @param bool $followRedirects Optional. Whether to follow redirects or not.
+     *                              Default is true.
+     */
+    public function followRedirects($followRedirects = true)
+    {
+        $this->getModule('PhpBrowser')->client->followRedirects($followRedirects);
+    }
 
-		// Check for 301 response code.
-		$this->assertEquals(301, $responseCode);
+    /**
+     * Check that a 200 HTTP Status is returned with the correct Location URL.
+     *
+     * Should be HTTP.
+     *
+     * @since 0.1.0
+     *
+     * @param string $url Relative or absolute URL of redirect destination.
+     */
+    public function seePermanentRedirectToHttpFor($url)
+    {
+        $this->permanentRedirectForProtocol($url, self::PROTOCOL_HTTP);
+    }
 
-		// Check location header URL contains submitted URL.
-		$this->assertContains($url, $locationHeader);
-	}
+    /**
+     * Check that a 301 HTTP Status is returned with the correct Location URL.
+     *
+     * @since 0.1.0
+     *
+     * @param string $url Relative or absolute URL of redirect destination.
+     */
+    public function seePermanentRedirectTo($url)
+    {
+        $response       = $this->getModule('PhpBrowser')->client->getInternalResponse();
+        $responseCode   = $response->getStatus();
+        $locationHeader = $response->getHeader('Location', true);
 
-	/**
-	 * Check that a 200 HTTP Status is returned with the correct Location URL.
-	 *
-	 * Should be HTTPS.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $url Relative or absolute URL of redirect destination.
-	 */
-	public function seePermanentRedirectToHttpsFor($url)
-	{
-		$this->permanentRedirectForProtocol($url, self::PROTOCOL_HTTPS);
-	}
+        // Check for 301 response code.
+        $this->assertEquals(301, $responseCode);
 
-	/**
-	 * Check that a 200 HTTP Status is returned with the correct Location URL.
-	 *
-	 * Should be HTTP.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $url Relative or absolute URL of redirect destination.
-	 */
-	public function seePermanentRedirectToHttpFor($url)
-	{
-		$this->permanentRedirectForProtocol($url, self::PROTOCOL_HTTP);
-	}
+        // Check location header URL contains submitted URL.
+        $this->assertContains($url, $locationHeader);
+    }
 
-	/**
-	 * Toggle redirections on and off.
-	 *
-	 * By default, BrowserKit will follow redirections, so to check for 30*
-	 * HTTP status codes and Location headers, they have to be turned off.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param bool $followRedirects Optional. Whether to follow redirects or not.
-	 *                              Default is true.
-	 */
-	public function followRedirects($followRedirects = true)
-	{
-		$this->getModule('PhpBrowser')->client->followRedirects($followRedirects);
-	}
+    /**
+     * Check that a 200 HTTP Status is returned with the correct Location URL.
+     *
+     * Should be HTTPS.
+     *
+     * @since 0.1.0
+     *
+     * @param string $url Relative or absolute URL of redirect destination.
+     */
+    public function seePermanentRedirectToHttpsFor($url)
+    {
+        $this->permanentRedirectForProtocol($url, self::PROTOCOL_HTTPS);
+    }
 
-	/**
-	 * Check that a 200 HTTP Status is returned with the correct Location URL.
-	 *
-	 * @param string $url      Relative or absolute URL of redirect destination.
-	 * @param string $protocol Protocol: 'http' or 'https'.
-	 */
-	protected function permanentRedirectForProtocol($url, $protocol)
-	{
-		$url = ltrim($url, '/');
-		$this->getModule('REST')->sendHead($url);
+    /**
+     * Check that a 200 HTTP Status is returned with the correct Location URL.
+     *
+     * @param string $url      Relative or absolute URL of redirect destination.
+     * @param string $protocol Protocol: 'http' or 'https'.
+     */
+    protected function permanentRedirectForProtocol($url, $protocol)
+    {
+        $url = ltrim($url, '/');
+        $this->getModule('REST')->sendHead($url);
 
-		$client       = $this->getModule('PhpBrowser')->client;
-		$responseCode = $client->getInternalResponse()->getStatus();
-		$responseUri  = $client->getHistory()->current()->getUri();
-		$scheme       = parse_url($responseUri, PHP_URL_SCHEME);
+        $client       = $this->getModule('PhpBrowser')->client;
+        $responseCode = $client->getInternalResponse()->getStatus();
+        $responseUri  = $client->getHistory()->current()->getUri();
+        $scheme       = parse_url($responseUri, PHP_URL_SCHEME);
 
-		// Check for 200 response code.
-		$this->assertEquals(200, $responseCode);
+        // Check for 200 response code.
+        $this->assertEquals(200, $responseCode);
 
-		// Check for submitted http/https value matches destination URL.
-		$this->assertEquals($protocol, $scheme);
+        // Check for submitted http/https value matches destination URL.
+        $this->assertEquals($protocol, $scheme);
 
-		// Check that destination URL contains submitted URL part.
-		$this->assertContains($url, $responseUri);
-	}
+        // Check that destination URL contains submitted URL part.
+        $this->assertContains($url, $responseUri);
+    }
 }
